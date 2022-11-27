a2db298c043e400270194d95e7294d2bc106a044
santialvesz <santialvesdsc@gmail.com>
2022-11-27 14:54:30 -0800

    Fixing some problems in amenity and place

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
+""" State Module for HBNB project """
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
+    """ class Amenity """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    # place_amenities = relationship("Place", secondary="place_amenity")
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   viewonly=True)

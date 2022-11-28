d1b6a457e42533bc094e6750ae841a7fcb025f6d
NachoCape <igcapezzolo@hotmail.com>
2022-11-28 17:21:41 +0000

    fix pycodestyle task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def Hello_HBNB():
     """starts a Flask web application that display Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """starts a Flask web application that display HBNB"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
